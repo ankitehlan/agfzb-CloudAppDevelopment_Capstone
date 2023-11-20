5d9eecbc4bb28b5d69d60fcb630726a49dbc1b69
Ankit Tehlan <ankitehlan@gmail.com>
2023-11-20 11:51:02 +0530

    Add django proxy service for cloud functions

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,6 +10,8 @@ from datetime import datetime
 import logging
 import json
 
+from djangoapp.restapis import get_dealers_from_cf
+
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
@@ -83,9 +85,14 @@ def registration_request(request):
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
-    context = {}
     if request.method == "GET":
-        return render(request, 'djangoapp/index.html', context)
+        url = "https://ankitehlanwo-3000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai/dealerships/get"
+        # Get dealers from the URL
+        dealerships = get_dealers_from_cf(url)
+        # Concat all dealer's short name
+        dealer_names = ' '.join([dealer.short_name for dealer in dealerships])
+        # Return a list of dealer short name
+        return HttpResponse(dealer_names)
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
